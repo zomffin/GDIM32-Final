2c0d819ee9d718732e5f66c7e372c0f00c3e38de
zom <matsunoisabel@gmail.com>
2026-02-17 18:43:48 -0800

    Player camera

--- GDIM32-Final/Assets/Player.cs
+++ GDIM32-Final/Assets/Player.cs
@@ -12,9 +12,10 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject targetPos; 
     
     [SerializeField] LayerMask raycastLayers;
-    
-    private float horizontal;
-    private float vertical;
+
+    private Vector3 _angles; 
+    private float _horizontal;
+    private float _vertical;
     
     // Start is called before the first frame update
     void Start()
@@ -25,9 +26,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
         
-        playerTransform.Translate((playerTransform.right * horizontal + playerTransform.forward * vertical) * _moveSpeed * Time.deltaTime);
+        playerTransform.Translate((playerTransform.right * _horizontal + playerTransform.forward * _vertical) * _moveSpeed * Time.deltaTime);
+
+        this.transform.position = playerTransform.position; 
+        
+        float rotationY = Input.GetAxis("Mouse Y") * _turnSpeed;
+        float rotationX = Input.GetAxis("Mouse X") * _turnSpeed;
+        
+        if (rotationY > 0)
+        {
+            _angles = new Vector3(Mathf.MoveTowards(_angles.x, -80, rotationY), _angles.y + rotationX, 0);
+        }
+        else
+        {
+            _angles = new Vector3(Mathf.MoveTowards(_angles.x, 80, -rotationY), _angles.y + rotationX, 0);
+        }
+        
+        transform.localEulerAngles = _angles;
+        transform.position = this.transform.position;
+        
     }
 }
