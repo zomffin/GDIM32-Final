3ff65ed258b42bb63646504bf38a95326efabf76
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-02-21 09:24:32 -0800

    Cauldron can detect collision object's name

--- GDIM32-Final/Assets/Scenes/Scripts/Cauldron.cs
+++ GDIM32-Final/Assets/Scenes/Scripts/Cauldron.cs
@@ -6,6 +6,10 @@ public class Cauldron : MonoBehaviour
 {
     void OnTriggerEnter(Collider collision)
     {
+        if (collision.name == "test item")
+        {
+            Debug.Log(collision.name);
+        }
         Debug.Log("Cooking");
     }
 }
