bd62c94acca2cddc73c8ee3dbdd3503b9cb5033f
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-02-25 12:00:25 -0800

    cauldron detect more than one item and can switch to win screen (collision weried sometimes)

--- GDIM32-Final/Assets/Scenes/Scripts/Cauldron.cs
+++ GDIM32-Final/Assets/Scenes/Scripts/Cauldron.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Cauldron : MonoBehaviour
 {
+    [SerializeField] int _itemcount = 0;
     void OnTriggerEnter(Collider collision)
     {
         if (collision.name == "test item")
         {
+            Destroy(collision.gameObject);
+            _itemcount++;
+            Debug.Log(_itemcount);
+        }
+
+    }
+    void Update()
+    {
+        if (_itemcount >= 2)
+        {
             SceneManager.LoadScene("WinScreen");
         }
-        Debug.Log("Cooking");
     }
 }
