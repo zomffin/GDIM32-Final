a3722cba0f0a1f5717935f11f7d8101c1f5d367a
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-03-16 16:30:05 -0700

    end dialogue after finish witch

--- GDIM32-Final/Assets/Scenes/Scripts/DialogueManager.cs
+++ GDIM32-Final/Assets/Scenes/Scripts/DialogueManager.cs
@@ -68,6 +68,7 @@ public class DialogueManager : MonoBehaviour
             else
             {
                 GameController.Instance.Cauldron.ActivateWitch();
+                EndDialogue();
             }
 
 
